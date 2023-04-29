73e8c68a288a0be8209c89b81b3d8e425a3866ba
Ray <dan@raydevs.com>
2023-04-29 18:11:31 +0000

    add api request to download the resume, fix grammar

---
+++ raydevsrs.Dockerfile
@@ -0,0 +1,29 @@
+# Build the React app
+FROM node:18 AS react-build
+WORKDIR /app
+COPY client/package*.json ./
+RUN npm install
+COPY client/ ./
+RUN npm run build
+
+# Build the Rust server
+FROM rust:1.69 AS rust-build
+WORKDIR /app
+COPY server/Cargo.toml server/Cargo.lock ./
+# Create a dummy main.rs to build the dependencies and cache them
+RUN mkdir -p src && \
+    echo 'fn main() {}' > src/main.rs && \
+    cargo build --release && \
+    rm -rf src
+COPY server/ ./
+RUN cargo build --release
+
+# Combine the React app and Rust server into the final image
+FROM debian:buster-slim
+RUN apt-get update && apt-get install -y libssl-dev ca-certificates && rm -rf /var/lib/apt/lists/*
+COPY --from=react-build /app/build /app/build
+COPY --from=rust-build /app/target/release/server /app/server
+COPY server/assets /app/assets 
+
+WORKDIR /app
+CMD ["/app/server"]
---
+++ server/src/main.rs
@@ -0,0 +1,50 @@
+use actix_files::Files;
+use actix_web::{App, HttpServer};
+use actix_web::{web, HttpResponse, Result};
+use actix_cors::Cors;
+use std::fs;
+
+async fn download_file() -> Result<HttpResponse> {
+    // Set the file path to be downloaded.
+    let file_path = "assets/dan_raymond_resume.pdf";
+
+    // Read the file contents.
+    let file_contents = fs::read(file_path)?;
+
+    // Create a response with the file contents and appropriate headers.
+    let response = HttpResponse::Ok()
+        .header("Content-Disposition", format!("attachment; filename=\"{}\"", "dan_raymond_resume.pdf"))
+        .content_type("application/octet-stream")
+        .body(file_contents);
+
+    Ok(response)
+}
+
+async fn run() -> std::io::Result<()> {
+    // Start the server with the provided address.
+    let addr = "0.0.0.0:3000";
+    println!("Serving on http://{}", addr);
+
+    HttpServer::new(|| {
+        let cors = Cors::permissive(); // You can customize the CORS settings if needed
+
+        App::new()
+        .wrap(cors)
+            // Serve files from the React build directory (e.g., "build" or "dist").
+        .route("/download", web::get().to(download_file))
+        .service(Files::new("/", "build").index_file("index.html"))
+        
+
+    })
+    .bind(addr)?
+    .run()
+    .await
+}
+
+#[actix_rt::main]
+async fn main() {
+    if let Err(err) = run().await {
+        eprintln!("Error: {}", err);
+        std::process::exit(1);
+    }
+}
